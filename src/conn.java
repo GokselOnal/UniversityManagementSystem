e75248a1f92e8b62e336262877a0c929e5b2a062
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-14 14:47:16 +0300

    Connection

--- src/conn.java
+++ src/conn.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 public class conn {
-    private static final String URL = "jdbc:mysql://localhost:3306/cs202_hw01?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
+    private static final String URL = "jdbc:mysql://localhost:3306/university?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "MyPass";
     Connection connection;
@@ -9,6 +9,7 @@ public class conn {
     conn(){
         try{
            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement = connection.createStatement();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
