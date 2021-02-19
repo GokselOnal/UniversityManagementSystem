55832534589def81a16692bc52046cc8b355f0de
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-19 20:04:54 +0300

    Student details

--- src/StudentDetails.java
+++ src/StudentDetails.java
@@ -4,17 +4,70 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 
-public class StudentDetails extends JFrame {
+public class StudentDetails extends JFrame implements ActionListener{
 
     JTable table;
     String x[] = {"Name","Age","E-mail","Address","Phone","Date of Birth","Level","Program"};
     String y[][];
+    JLabel label_level, label_department;
+    JComboBox levels,departments;
+    JButton search;
 
     StudentDetails(){
         this.setTitle("Student Details");
-        this.setSize(1250,430);
+        this.setSize(1250,500);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
+
+        label_level = new JLabel("Level");
+        label_level.setBounds(25,20,50,30);
+
+        String levelsArr[] = new String[getLevelCount()];
+        try{
+            conn connection = new conn();
+            String selectQuery = "select * from level";
+            ResultSet rs = connection.statement.executeQuery(selectQuery);
+            int i = 0;
+            while(rs.next()){
+                levelsArr[i] = rs.getString("level_name");
+                i++;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        levels = new JComboBox(levelsArr);
+        levels.setBounds(70,20,150,30);
+        levels.setBackground(Color.white);
+
+        label_department = new JLabel("Department");
+        label_department.setBounds(300,20,70,30);
+
+
+        String departmentsArr[] = new String[getDepartmentCount()];
+        try{
+            conn connection = new conn();
+            String selectQuery = "select * from department";
+            ResultSet rs = connection.statement.executeQuery(selectQuery);
+            int i = 0;
+            while(rs.next()){
+                departmentsArr[i] = rs.getString("department_name");
+                i++;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        departments = new JComboBox(departmentsArr);
+        departments.setBounds(380,20,150,30);
+        departments.setBackground(Color.white);
+
+        search = new JButton("Search");
+        search.setBounds(650,20,120,30);
+        search.setFont(new Font("serif",Font.BOLD,15));
+        search.addActionListener(this);
+        search.setBackground(Color.white);
+        search.setForeground(Color.BLACK);
+        search.setFocusable(false);
 
         try{
            conn connection = new conn();
@@ -35,8 +88,6 @@ public class StudentDetails extends JFrame {
 
                conn connection2 = new conn();
                String level_id = rs.getString("level_id");
-               System.out.println(level_id);
-               System.out.println(y[i][0]);
                String selectQueryLevel = "select * from level where lid= '"+level_id+"'";
                ResultSet rsLevel = connection2.statement.executeQuery(selectQueryLevel);
                rsLevel.next();
@@ -63,11 +114,17 @@ public class StudentDetails extends JFrame {
         }
 
         JScrollPane sp = new JScrollPane(table);
-        sp.setBounds(20,20,1200,330);
+        sp.setBounds(20,100,1200,330);
 
         this.setResizable(false);
         this.getContentPane().setBackground(new Color(255,140,0));
+        this.add(label_level);
+        this.add(levels);
+        this.add(label_department);
+        this.add(departments);
+        this.add(search);
         this.add(sp);
+        this.setVisible(true);
     }
 
     public int getStudentCount(){
@@ -84,7 +141,41 @@ public class StudentDetails extends JFrame {
         }
         return studentCount;
     }
+
+    public int getLevelCount(){
+        int levelCount = 0;
+        try{
+            conn connection = new conn();
+            String selectQuery = "select count(lid) as count from level";
+            ResultSet rs= connection.statement.executeQuery(selectQuery);
+            rs.next();
+            levelCount += rs.getInt("count");
+
+        }catch (Exception ee){
+            ee.printStackTrace();
+        }
+        return levelCount;
+    }
+    public int getDepartmentCount(){
+        int depCount = 0;
+        try{
+            conn connection = new conn();
+            String selectQuery = "select count(did) as count from department";
+            ResultSet rs= connection.statement.executeQuery(selectQuery);
+            rs.next();
+            depCount += rs.getInt("count");
+
+        }catch (Exception ee){
+            ee.printStackTrace();
+        }
+        return depCount;
+    }
     public static void main(String[] args) {
         new StudentDetails();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
