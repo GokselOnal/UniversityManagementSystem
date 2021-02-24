bdd9526f452ead60cb8fa073fcb28d2144110359
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-24 13:52:54 +0300

    My schedule

--- src/MySchedule.java
+++ src/MySchedule.java
@@ -10,7 +10,7 @@ public class MySchedule extends JFrame {
     String y[][];
     MySchedule(){
         this.setTitle("My Schedule");
-        this.setSize(1000,800);
+        this.setSize(1000,300);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -43,18 +43,38 @@ public class MySchedule extends JFrame {
             while (rs2.next()){
                 courseTaken.add(rs2.getString("courses"));
             }
-            /*for(int i = 0 ; i < courseTaken.size(); i++){
-                System.out.println(courseTaken.get(i));
-            }*/
 
-            //sqlden indexleri alıp programda celleri doldurr
+            conn connection3 = new conn();
+
+            conn connection4 = new conn();
+            for(String i : courseTaken) {
+                String selectCells = "select dayIndex, timeIndex from schedule where user_id = '"+userId+"' and course_id = '"+i+"'";
+
+                ResultSet rs3 = connection3.statement.executeQuery(selectCells);
+                ArrayList<Integer> timeIndexes = new ArrayList<Integer>();
+
+                ResultSet rs4 = connection4.statement.executeQuery("select duration from course where cid = '"+i+"'");
+                rs4.next();
+                int duration = rs4.getInt("duration");
+                int dayIndex = 0;
+                while (rs3.next()){
+                    timeIndexes.add(rs3.getInt("timeIndex"));
+                    dayIndex += rs3.getInt("dayIndex");
+                }
+               int day_index = dayIndex/duration;
+
+                for (int time : timeIndexes){
+                    y[time - 1][day_index+1] = i;
+                }
+            }
+
             table = new JTable(y, x);
             table.setGridColor(Color.orange);
         }catch (Exception e){
             e.printStackTrace();
         }
         sp = new JScrollPane(table);
-        sp.setBounds(20,20,900,210);
+        sp.setBounds(40,20,900,205);
 
         table.setEnabled(false);
         this.getContentPane().setBackground(new Color(255,140,0));
